21dcd610b19140159a1def35bf02023c46e3817f
LJH <LJH@DESKTOP-juno>
2017-01-04 17:13:36 +0900

    insert

--- src/main/java/kr/or/dgit/bigdata/mybatis_dev/mappers/StudentMapper.java
+++ src/main/java/kr/or/dgit/bigdata/mybatis_dev/mappers/StudentMapper.java
@@ -4,5 +4,6 @@ import kr.or.dgit.bigdata.mybatis_dev.dto.Student;
 
 public interface StudentMapper {
 	Student findStudentById(int id);
-	
+
+	int insertStudent(Student student);
 }
--- src/test/java/kr/or/dgit/bigdata/mybatis_dev/StudentServiceTest.java
+++ src/test/java/kr/or/dgit/bigdata/mybatis_dev/StudentServiceTest.java
@@ -1,18 +1,19 @@
 package kr.or.dgit.bigdata.mybatis_dev;
 
-import static org.junit.Assert.*;
+import java.util.Date;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import kr.or.dgit.bigdata.mybatis_dev.dto.PhoneNumber;
 import kr.or.dgit.bigdata.mybatis_dev.dto.Student;
 import kr.or.dgit.bigdata.mybatis_dev.service.StudentService;
 
 public class StudentServiceTest {
 	private static StudentService studentService;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		studentService = StudentService.getInstance();
@@ -28,6 +29,20 @@ public class StudentServiceTest {
 		Student student = studentService.findStudentById(1);
 		Assert.assertNotNull(student);
 		System.out.println("testFindStudentById" + student);
+	}
+
+	@Test
+	public void testInsertStudent() {
+		Student student = new Student();
+		student.setStudId(3);
+		student.setName("김주하");
+		student.setEmail("kjh@gmail.com");
+		student.setPhone(new PhoneNumber("010-1234-5678"));
+		student.setDob(new Date());
+
+		int result = studentService.insertStudent(student);
+		System.out.printf("testInsertStudent %s : result %d%n", student, result);
+		Assert.assertSame(1, result);
 	}
 
 }
